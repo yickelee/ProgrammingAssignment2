4397dca631859116b5137dae6df6ef02b064f233
Yick E Lee <yicklee@gmail.com>
2014-08-10 15:17:44 -0700

    added CacheMatrix files

---
+++ cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x,...){
+        inv <- x$getinv()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+}
